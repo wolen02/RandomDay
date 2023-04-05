aa7be44a2b209a3a29007ed0dd91662cfc9355f3
주지성\wolen <wnwltjd333@naver.com>
2023-04-05 21:42:03 +0900

    [주지성] - 배포 과정중 발생한 에러 관한 내용 수정

--- src/main/java/com/wolen/randomday/RandomDayApplication.java
+++ src/main/java/com/wolen/randomday/RandomDayApplication.java
@@ -2,9 +2,10 @@ package com.wolen.randomday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RandomDayApplication {
+public class RandomDayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandomDayApplication.class, args);
